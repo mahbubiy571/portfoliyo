74aa86bb73a4a3a412318cf710e60e5b7e6f603f
avazbek <avazbek@gmail.com>
2025-04-07 18:53:19 +0500

    loyiha 1 loyiha 2 added

---
+++ js/data.js
@@ -0,0 +1,14 @@
+export default [
+
+  {
+    title: "Loyiha 1",
+    github: "https://github.com/mahbubiy571/Loyiha-1.git",
+    vercel: "https://loyiha-1-seven.vercel.app/",
+  },
+
+  {
+    title: "Loyiha 2",
+    github: "https://github.com/mahbubiy571/Loyiha-2.git",
+    vercel: "https://loyiha-2-delta.vercel.app/",
+  },
+];
