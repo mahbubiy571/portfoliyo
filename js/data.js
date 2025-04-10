e2d0bebd24b5d5e497deed89594e88b79e98c146
avazbek <avazbek@gmail.com>
2025-04-11 01:09:53 +0500

    ..

--- js/data.js
+++ js/data.js
@@ -1,16 +1,31 @@
 export default [
   {
+    title: "Sports Car",
+    github: "https://github.com/mahbubiy571/sports-car.git",
+    vercel: "https://sports-car-gamma.vercel.app/",
+  },
+  {
+    title: "iPhone",
+    github: "https://github.com/mahbubiy571/apple-phone.git",
+    vercel: "https://apple-phone-zeta.vercel.app/",
+  },
+  {
+    title: "Watch",
+    github: "https://github.com/mahbubiy571/soat.git",
+    vercel: "https://soat-beta.vercel.app/",
+  },
+  {
     title: "intro-section-with-dropdown-navigation",
     github: "https://github.com/mahbubiy571/8-dars.git",
     vercel: "https://intro-section-with-dropdown-navigation-iota-six.vercel.app/",
   },
   {
-    title: "Loyiha 1",
+    title: "blog-preview-card",
     github: "https://github.com/mahbubiy571/Loyiha-1.git",
     vercel: "https://loyiha-1-seven.vercel.app/",
   },
   {
-    title: "Loyiha 2",
+    title: "social-links-profile",
     github: "https://github.com/mahbubiy571/Loyiha-2.git",
     vercel: "https://loyiha-2-delta.vercel.app/",
   },
