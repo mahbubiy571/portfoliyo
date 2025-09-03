b1a9e73ee0683e80ac2dc9f48d76b6c1ba5876b5
avazbek <avazbek@gmail.com>
2025-09-03 17:45:46 +0500

    ..

--- js/data.js
+++ js/data.js
@@ -2,7 +2,7 @@ export default [
   {
     title: "Authify",
     github: "https://github.com/mahbubiy571/Authify",
-    vercel: "https://authify-437rxlgwr-mahbubiy571s-projects.vercel.app",
+    vercel: "https://authify-tau.vercel.app/login",
   },
   {
     title: "Todo App",
